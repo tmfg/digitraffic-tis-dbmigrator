92399f7f077900ff67c1a895491f1d2d9a9b767b
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2023-06-26 17:32:18 +0300

    Simplifying table names by removing subsystem prefixes

--- db/migrations/R__seed_data.sql
+++ db/migrations/R__seed_data.sql
@@ -1,16 +1,16 @@
-INSERT INTO tis_organization (business_id, name)
+INSERT INTO organization (business_id, name)
      VALUES ('2942108-7', 'Fintraffic Oy')
 ON CONFLICT (business_id)
          DO UPDATE SET name = 'Fintraffic Oy';
 
 INSERT INTO ruleset (owner_id, category, identifying_name, description, type)
-     VALUES ((SELECT id FROM tis_organization WHERE business_id = '2942108-7'),
+     VALUES ((SELECT id FROM organization WHERE business_id = '2942108-7'),
              'generic',
              'gtfs.canonical.v4_0_0',
              'Canonical GTFS Validator by MobilityData, version v4.0.0',
              'validation_syntax')
 ON CONFLICT (identifying_name)
          DO UPDATE SET category = 'generic',
-                       owner_id = (SELECT id FROM tis_organization WHERE business_id = '2942108-7'),
+                       owner_id = (SELECT id FROM organization WHERE business_id = '2942108-7'),
                        description = 'Canonical GTFS Validator by MobilityData, version v4.0.0',
                        type = 'validation_syntax';
