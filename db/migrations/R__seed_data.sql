85b74dfe7d27aaefd67b0ebd74193efa8bcfd854
Esko Suomi <esko.suomi@solita.fi>
2023-10-18 14:11:06 +0300

    make only the latest Canonical GTFS validator to be forced to execute for GTFS
    This ruleset picking logic will need some refinement in the future

--- db/migrations/R__seed_data.sql
+++ db/migrations/R__seed_data.sql
@@ -3,6 +3,6 @@ INSERT INTO organization (business_id, name)
 ON CONFLICT (business_id)
          DO UPDATE SET name = 'Fintraffic Oy';
 
-SELECT upsert_ruleset('2942108-7', 'gtfs.canonical.v4_0_0', 'Canonical GTFS Validator by MobilityData, version v4.1.0', 'validation_syntax');
+SELECT upsert_ruleset('2942108-7', 'gtfs.canonical.v4_0_0', 'Canonical GTFS Validator by MobilityData, version v4.0.0', 'validation_syntax', 'specific');
 SELECT upsert_ruleset('2942108-7', 'gtfs.canonical.v4_1_0', 'Canonical GTFS Validator by MobilityData, version v4.1.0', 'validation_syntax');
 SELECT upsert_ruleset('2942108-7', 'netex.entur.v1_0_1', 'NeTEx Validator by Entur, version v1.0.1', 'validation_syntax');
