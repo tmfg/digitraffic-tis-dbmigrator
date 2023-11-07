d9a5cff7242f498f72108a7f00c52158dbc58bb9
Esko Suomi <esko.suomi@solita.fi>
2023-11-07 18:05:35 +0200

    first draft support for netex2gtfs external rule
    This implementation doesn't do anything specifically yet, but it contains all the bits needed for rule execution. Code cleanup is also included in this commit.

--- db/migrations/R__seed_data.sql
+++ db/migrations/R__seed_data.sql
@@ -46,3 +46,4 @@ $$;
 SELECT upsert_ruleset('2942108-7', 'gtfs', 'gtfs.canonical.v4_0_0', 'Canonical GTFS Validator by MobilityData, version v4.0.0', 'validation_syntax', 'specific', ARRAY ['prepare.download', 'validate']);
 SELECT upsert_ruleset('2942108-7', 'gtfs', 'gtfs.canonical.v4_1_0', 'Canonical GTFS Validator by MobilityData, version v4.1.0', 'validation_syntax', 'generic', ARRAY ['prepare.download', 'validate']);
 SELECT upsert_ruleset('2942108-7', 'netex', 'netex.entur.v1_0_1', 'NeTEx Validator by Entur, version v1.0.1', 'validation_syntax', 'generic', ARRAY ['prepare.download', 'validate']);
+SELECT upsert_ruleset('2942108-7', 'netex', 'netex2gtfs.entur.v2_0_6', 'NeTEx to GTFS Converter by Entur, version v2.0.6', 'conversion_syntax', 'generic', ARRAY ['prepare.download', 'netex.entur.v1_0_1', 'validate', 'convert']);
