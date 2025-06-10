1be789046d821cc6b5f4c00bf50df6fcc731b3e1
Topi Penttinen <212693087+solita-topip@users.noreply.github.com>
2025-06-10 15:24:06 +0300

    Add roles-column into company table

---
+++ db/migrations/V41__add_company_roles.sql
@@ -0,0 +1,8 @@
+-- # adding company roles to company
+ALTER TABLE company
+    ADD COLUMN IF NOT EXISTS roles TEXT[] DEFAULT '{"operator"}';
+
+-- # add authority role to existing authorities
+UPDATE company SET roles = array_append(roles, 'authority') WHERE id IN (
+    SELECT DISTINCT partner_a_id FROM partnership WHERE partnership.type = 'authority-provider'
+    ) AND NOT company.roles @> ARRAY['authority'];
