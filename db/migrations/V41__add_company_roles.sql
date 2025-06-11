52cc886b9dfdcd7c6e827f43f44fce848ae9e60d
Topi Penttinen <212693087+solita-topip@users.noreply.github.com>
2025-06-11 12:39:41 +0300

    Reformat UPDATE -statement
    Co-authored-by: Esko Suomi <esko.suomi@solita.fi>

--- db/migrations/V41__add_company_roles.sql
+++ db/migrations/V41__add_company_roles.sql
@@ -3,6 +3,9 @@ ALTER TABLE company
     ADD COLUMN IF NOT EXISTS roles TEXT[] DEFAULT '{"operator"}';
 
 -- # add authority role to existing authorities
-UPDATE company SET roles = array_append(roles, 'authority') WHERE id IN (
-    SELECT DISTINCT partner_a_id FROM partnership WHERE partnership.type = 'authority-provider'
-    ) AND NOT company.roles @> ARRAY['authority'];
+UPDATE company
+   SET roles = ARRAY_APPEND(roles, 'authority')
+ WHERE id IN (SELECT DISTINCT partner_a_id
+                FROM partnership
+               WHERE partnership.type = 'authority-provider')
+   AND NOT company.roles @> ARRAY ['authority'];
